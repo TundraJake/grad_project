a4faa54e8de2e815ab1a758ac53d5e02a6421914
develjacob <mcjacob61@gmail.com>
2018-03-19 17:39:41 -0800

    add journal, update for special words

--- tweep.py
+++ tweep.py
@@ -34,4 +34,8 @@ public_tweets = api.home_timeline()
     # print(tweet.text)
 
 myStream = tweepy.Stream(auth = api.auth, listener=streamer)
-myStream.filter(track=['python'])
+# Idea: Use newspapers and keywords to fill lists and listen
+# I'll have to figure out what I need to do to get special keywords, this might 
+# include a very generic approach.
+special_words = ['DOW', 'S&P500', 'APPL']
+myStream.filter(track=['DOW'])
