53227b59c9722827efa87ede7adb3e78de2fe77c
develjacob <mcjacob61@gmail.com>
2018-03-22 16:05:39 -0800

    fix authenticate function, now runs

--- tweep.py
+++ tweep.py
@@ -18,24 +18,27 @@ class TweetStreamListener(tweepy.StreamListener):
         print(status.text)
 
 
+def strip_list_newline(str_to_strip):
+	for ii in range(len(str_to_strip)):
+		str_to_strip[ii] = str_to_strip[ii].strip()
+
+def authenticate(keys):
+	auth = tweepy.OAuthHandler(key[0], key[1])
+	auth.set_access_token(key[2], key[3])
+	api = tweepy.API(auth)
+	return api
+
 streamer = TweetStreamListener()
-
 key = list(open('keys.txt'))
-for k in range(len(key)):
-	key[k] = key[k].strip()
+strip_list_newline(key)
+api = authenticate(key)
 
-auth = tweepy.OAuthHandler(key[0], key[1])
-auth.set_access_token(key[2], key[3])
-
-api = tweepy.API(auth)
-
-public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-    # print(tweet.text)
 
 myStream = tweepy.Stream(auth = api.auth, listener=streamer)
 # Idea: Use newspapers and keywords to fill lists and listen
 # I'll have to figure out what I need to do to get special keywords, this might 
 # include a very generic approach.
+# Use below of async connection, otherwise main thread is used. 
+# myStream.filter(track=['python'], async=True) 
 special_words = ['DOW', 'S&P500', 'APPL']
-myStream.filter(track=['DOW'])
+myStream.filter(track=special_words)
