f0235f96edba38dc5f9f6ca485055e61706ddf9c
develjacob <mcjacob61@gmail.com>
2018-03-22 16:12:12 -0800

    remove print statement, refactor authentication all to authentication function

--- tweep.py
+++ tweep.py
@@ -22,23 +22,27 @@ def strip_list_newline(str_to_strip):
 	for ii in range(len(str_to_strip)):
 		str_to_strip[ii] = str_to_strip[ii].strip()
 
-def authenticate(keys):
+def authenticate():
+	key = list(open('keys.txt'))
+	strip_list_newline(key)
 	auth = tweepy.OAuthHandler(key[0], key[1])
 	auth.set_access_token(key[2], key[3])
 	api = tweepy.API(auth)
 	return api
 
-streamer = TweetStreamListener()
-key = list(open('keys.txt'))
-strip_list_newline(key)
-api = authenticate(key)
+def main():
+	streamer = TweetStreamListener()
+	api = authenticate()
+
+	myStream = tweepy.Stream(auth = api.auth, listener=streamer)
+	# Idea: Use newspapers and keywords to fill lists and listen
+	# I'll have to figure out what I need to do to get special keywords, this might 
+	# include a very generic approach.
+	# Use below of async connection, otherwise main thread is used. 
+	# myStream.filter(track=['python'], async=True) 
+	special_words = ['DOW', 'S&P500', 'APPL', 'GOOG', 'TWTR']
+	myStream.filter(track=special_words)
 
 
-myStream = tweepy.Stream(auth = api.auth, listener=streamer)
-# Idea: Use newspapers and keywords to fill lists and listen
-# I'll have to figure out what I need to do to get special keywords, this might 
-# include a very generic approach.
-# Use below of async connection, otherwise main thread is used. 
-# myStream.filter(track=['python'], async=True) 
-special_words = ['DOW', 'S&P500', 'APPL']
-myStream.filter(track=special_words)
+if __name__ == '__main__':
+	main()
