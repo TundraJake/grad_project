c39845b2eb64e9e53f656387f1776d15750662a0
develjacob <mcjacob61@gmail.com>
2018-03-28 16:11:08 -0800

    add nn testing

--- nn.py
+++ nn.py
@@ -40,8 +40,31 @@ class Test(unittest.TestCase):
 		predictions = model.predict(X)
 		# round predictions
 		rounded = [round(x[0]) for x in predictions]
-		self.__class__.test_networks.append(1)
-		print(rounded)
+		self.__class__.test_networks.append(rounded)
+
+	def test_small_nn(self):
+		seed = 1
+		numpy.random.seed(seed)
+		# load pima indians dataset
+		dataset = numpy.loadtxt("diab.csv", delimiter=",")
+		# split into input (X) and output (Y) variables
+		X = dataset[:,0:8]
+		Y = dataset[:,8]
+		# create model
+		model = Sequential()
+		model.add(Dense(8, input_dim=8, init='uniform', activation='relu'))
+		model.add(Dense(32, init='uniform', activation='sigmoid'))
+		model.add(Dense(1, init='uniform', activation='sigmoid'))
+		# Compile model
+		model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+		# Fit the model
+		model.fit(X, Y, epochs=250, batch_size=10,  verbose=2)
+		# calculate predictions
+		predictions = model.predict(X)
+		# round predictions
+		rounded = [round(x[0]) for x in predictions]
+		self.__class__.test_networks.append(rounded)
+
 
 
 
