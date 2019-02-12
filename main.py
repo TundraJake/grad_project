46bc69da83814ea5fb3aa96a02107ae5f7ddb4c9
Jacob McKenna <mcjacob61@gmail.com>
2019-02-11 15:18:04 -0900

    working time series nn, still working on classifier nn for tweets

--- main.py
+++ main.py
@@ -5,66 +5,190 @@ UAF Graduate Project
 main.py - Starting point of the entire project.
 
 '''
+
 from neural_network import *
+from classifier_nn import *
 from online_data_fetcher import * 
 import pandas as pd
+
+from sklearn.feature_extraction.text import CountVectorizer
+# create CountVectorizer object
+vectorizer = CountVectorizer()
 
 # from sklearn.feature_extraction import DictVectorizer
 # vec = DictVectorizer()
 
-
-def main():
-
-    # X = np.array([[1,1,1,1,1,1,0,0,0,0,1,1],
-    #                 [1,1,1,1,1,1,0,0,0,0,1,1],
-    #                 [1,1,1,1,1,1,0,0,0,0,1,1],
-    #                 [1,1,1,0,1,1,0,0,0,0,1,1],
-    #                 [1,0,1,1,1,1,0,0,0,0,1,1],
-    #                 [1,1,0,1,0,1,0,0,0,0,1,1],
-    #                 [1,1,1,1,0,1,0,0,0,0,1,1]])
-
-    # Y = np.array([1,0,0,1,1,0,1,])
-
-    # X = encoded_docs
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://stock:money@localhost:5432/stock_market_data')
 
 
-
-
-    data = pd.read_csv('data/numerical/set_1_stocks.csv')
-
+def lstm_sp500_model():
+    data = pd.read_csv('data/numerical/sp500.csv')
+    # data = pd.read_sql_query("select * from \"AAPL_ticker\"", con=engine)
+    # print(type(data))
     data = data.drop(['DATE'], 1)
+    
     n = data.shape[0] 
-    p = data.shape[1]
 
+    # Scale data
+    from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder as le
+    scaler = MinMaxScaler(feature_range = (0, 1))
     data = data.values
+    data = scaler.fit_transform(data)
 
     train_start = 0
     train_end = int(np.floor(0.8*n))
+
     test_start = train_end
     test_end = n
 
     data_train = data[np.arange(train_start, train_end), :]
     data_test = data[np.arange(test_start, test_end), :]
 
-    # Scale data
-    from sklearn.preprocessing import MinMaxScaler, StandardScaler
-    scaler = MinMaxScaler()
-    data_train = scaler.fit_transform(data_train)
-    data_test = scaler.transform(data_test)
-
     X = data_train[:, 1:]
+    X = np.reshape(X, (X.shape[0], X.shape[1], 1))
     Y = data_train[:, 0]
 
     X_test = data_test[:, 1:]
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
     Y_test = data_test[:, 0]
 
-    print("X Shape: ", X.shape[1])
-    example_nn = Neural_Network(X.shape[1])
+    example_nn = Neural_Network(X, Y, X_test, Y_test)
 
-    example_nn.summary()
-    example_nn.set_data(X, Y, X_test, Y_test)
+    # example_nn.train()
+    # example_nn.write_weights_to_h5()
+    # example_nn.evaluate()
 
-    example_nn.train()
+    example_nn.create_and_load_model('results/lstm_90lr_sp500_weights.h5')
+    prediction = example_nn.predict()
+    # prediction = le.inverse_transform(prediction)
+    
+    print(type(X_test))
+
+    # print(X_test[0])
+    plt.plot(prediction)
+    plt.plot(Y_test)
+    plt.title('SP500 Prediction vs Actual')
+    plt.ylabel('pred (%)')
+    plt.xlabel('60 second ticks')
+    plt.legend(['prediction', 'actual'], loc='upper left')
+    plt.show()
+
+def lstm_01lr_sp500_model():
+    data = pd.read_csv('data/numerical/sp500.csv')
+    # data = pd.read_sql_query("select * from \"AAPL_ticker\"", con=engine)
+    # print(type(data))
+    data = data.drop(['DATE'], 1)
+    
+    n = data.shape[0] 
+
+    # Scale data
+    from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder as le
+    scaler = MinMaxScaler(feature_range = (0, 1))
+    data = data.values
+    data = scaler.fit_transform(data)
+
+    train_start = 0
+    train_end = int(np.floor(0.8*n))
+
+    test_start = train_end
+    test_end = n
+
+    data_train = data[np.arange(train_start, train_end), :]
+    data_test = data[np.arange(test_start, test_end), :]
+
+    X = data_train[:, 1:]
+    X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+    Y = data_train[:, 0]
+
+    X_test = data_test[:, 1:]
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+    Y_test = data_test[:, 0]
+
+    example_nn = Neural_Network(X, Y, X_test, Y_test)
+
+    # example_nn.train()
+    # example_nn.write_weights_to_h5()
+    # example_nn.evaluate()
+
+    # example_nn.write_history_to_file()
+    example_nn.create_and_load_model('model.h5')
+    prediction = example_nn.predict()
+    # prediction = le.inverse_transform(prediction)
+    
+    print(type(X_test))
+
+    # print(X_test[0])
+    plt.plot(prediction)
+    plt.plot(Y_test)
+    plt.title('SP500 Prediction vs Actual')
+    plt.ylabel('pred (%)')
+    plt.xlabel('minutes')
+    plt.legend(['prediction', 'actual'], loc='upper left')
+    plt.show()
+
+
+# from tensorflow.python.client import device_lib
+
+# def get_available_gpus():
+#     local_device_protos = device_lib.list_local_devices()
+#     return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
+def curate_aapl_data():
+    data = pd.read_csv('data/numerical/Apple-Twitter-Sentiment-DFE.csv', encoding='ISO-8859-1')
+    print(data.columns.values)
+
+    print('\n\n\n')
+    data = data.drop(['_unit_state', 
+                    'Unnamed: 12', 
+                    'Unnamed: 13',
+                    'sentiment_gold',
+                    'query',
+                    'id',
+                    '_last_judgment_at',
+                    'date'], 1)
+
+    mask = data.sentiment == 'not_relevant'
+    column_name = "sentiment"
+    data.loc[mask, column_name] = 0
+
+    data.to_csv('data/numerical/revamped_aapl.csv', sep=',', encoding='ISO-8859-1')
+
+
+def lstm_01lr_aapl_sentiment():
+    data = pd.read_csv('data/numerical/revamped_aapl.csv', encoding='ISO-8859-1')
+
+    from keras.preprocessing.text import Tokenizer
+    t = Tokenizer()
+
+    tweets = data['text']
+    data = data.drop(['text', '_unit_id', 'Unnamed: 0'], 1)
+    t.fit_on_texts(tweets)
+    # print(tweets)
+    encoded_docs = t.texts_to_matrix(tweets, mode='count')
+    print(encoded_docs)
+    print(encoded_docs)
+    # Convert from pd.df to numpy array
+
+    # print(vectorizer.vocabulary_)
+    
+    # data = np.append(data, encoded_docs, 1)
+    print(data)
+    
+    from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder as le
+    scaler = MinMaxScaler(feature_range = (0, 1))
+    data = data.values
+    data = scaler.fit_transform(data)
+    print(data)
+
+
+
+def main():
+    # lstm_sp500_model()
+    # lstm_01lr_sp500_model()
+    # get_available_gpus()
+    lstm_01lr_aapl_sentiment()
+
 
 if __name__ == "__main__":
     main()
--- neural_network.py
+++ neural_network.py
@@ -5,67 +5,129 @@ UAF Graduate Project
 nn.py - Keras NN market prediction.
 
 '''
+
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.text import text_to_word_sequence, one_hot
+from keras.utils import multi_gpu_model
+from keras.callbacks import ModelCheckpoint
 import keras
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout, LSTM
+import matplotlib.pyplot as plt 
 import numpy.random as npr
 import numpy as np
 import nltk
 
-# # from tensorflow.python.client import device_lib
-# # print(device_lib.list_local_devices())
 
+
+# from tensorflow.python.client import device_lib
+# import tensorflow as tf
+# print(device_lib.list_local_devices())
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# print(tf.test.is_gpu_available())
+# from keras import backend as K
+# K.tensorflow_backend._get_available_gpus()
 
 # from keras import backend as K
 # print(K.tensorflow_backend._get_available_gpus())
-# # config = ConfigProto( device_count = {'GPU': 1} )
-# # sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-# # keras.backend.set_session(sess) 
+# config = ConfigProto( device_count = {'GPU': 1} )
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# keras.backend.set_session(sess) 
 
 class Neural_Network():
 
-	def __init__(self, input_shape):
+	def __init__(self, X, Y, X_test, Y_test):
 		npr.seed(1)
-		self.model = Sequential()
-		self.input_shape_ = input_shape
-		self.x_train_ = None
-		self.y_train_ = None
+		
+		self.x_train_ = X
+		self.y_train_ = Y
 
-		self.x_test_ = None
-		self.y_test_ = None
+		self.x_test_ = X_test
+		self.y_test_ = Y_test
 
-		self.__add_layers()
+		self.model = self.__add_lstm_90lr_sp500_layers()
 
-	def __add_layers(self):
-		self.model.add(Dense(1024, use_bias=True, bias_initializer='zeros', input_shape=(self.input_shape_,), activation='relu'))
-		# self.model.add(Dense(1024, activation='relu'))
-		self.model.add(Dense(512, activation='relu'))
-		self.model.add(Dense(256, activation='relu'))
-		self.model.add(Dense(128, activation='relu'))
-		self.model.add(Dense(1, activation='relu'))
+		self.history_ = None
+
+	def __built_model(self):
+		model = Sequential()
+		model.add(LSTM(units = 50, return_sequences = True, input_shape = (self.x_train_.shape[1], 1)))
+		model.add(Dropout(0.2))
+		model.add(LSTM(units = 50, return_sequences = True))
+		model.add(Dropout(0.2))
+		model.add(LSTM(units = 50, return_sequences = True))
+		model.add(Dropout(0.2))
+		model.add(LSTM(units = 50))
+		model.add(Dropout(0.2))
+		model.add(Dense(units = 1))
+		return model
+
+
+	def __add_lstm_90lr_sp500_layers(self):
+		model = Sequential()
+		model.add(LSTM(units = 50, return_sequences = True, input_shape = (self.x_train_.shape[1], 1)))
+		model.add(Dropout(0.2))
+		model.add(LSTM(units = 50, return_sequences = True))
+		model.add(Dropout(0.2))
+		model.add(LSTM(units = 50, return_sequences = True))
+		model.add(Dropout(0.2))
+		model.add(LSTM(units = 50))
+		model.add(Dropout(0.2))
+		model.add(Dense(units = 1))
+		return model
 
 	def get_x_training_set(self):
 		return self.x_train_
 
+
+
 	def get_y_training_set(self):
 		return self.y_train_
 
-	def set_data(self, x_train, y_train, x_test, y_test):
-		self.x_train_ = x_train
-		self.y_train_ = y_train
-		self.x_test_ = x_test
-		self.y_test_ = y_test
+	def get_history(self):
+		return self.history_.history
+
+	def write_loss_history_graph(self):
+		hist = self.get_history()
+		plt.plot(hist['loss'])
+		plt.plot(hist['val_loss'])
+		plt.title('model loss')
+		plt.ylabel('loss')
+		plt.xlabel('epoch')
+		plt.legend(['train', 'test'], loc='upper left')
+		savefig('results/lstm_loss_01lr_sp500.png')
+		plt.clf()
+
 
 	def train(self):
 		opt = keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9,)
-		self.model.compile(loss='mean_squared_error', optimizer=opt,  metrics=['accuracy'])
-		self.model.fit(self.x_train_, self.y_train_, epochs=10, batch_size=256, validation_data=(self.x_test_, self.y_test_))
+		self.model.compile(loss='mean_squared_error', optimizer='adam')
+		self.__checkpoint()
+		self.history_ = self.model.fit(self.get_x_training_set(), self.get_y_training_set(), epochs=40, batch_size=3000, verbose=1)
+		# print(self.history_.history.keys())
 
-	def evaulate(self):
-		scores = self.model.evaluate(self.x_train_, self.y_train_, verbose=0)
-		print("%s: %.2f%%" % (self.model.metrics_names[1], scores[1]*100))
+	def __checkpoint(self):
+		"""
+		Saves data between epochs.
+		"""
+		checkpoint = ModelCheckpoint("results/best_weights_lstm_01lr_sp500.h5", monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+		self.callbacks_list = [checkpoint]
+
+	def evaluate(self):
+		scores = self.model.evaluate(self.x_test_, self.y_test_)
+		# summarize history for loss
+		plt.plot(self.history_.history['loss'])
+		# plt.plot(self.history_.history['val_loss'])
+		plt.title('model loss')
+		plt.ylabel('loss')
+		plt.xlabel('epoch')
+		plt.legend(['train', 'test'], loc='upper left')
+		plt.show()
+		# print("%s: %.2f%%" % (self.model.metrics_names[1], scores[1]*100))
+
+	def create_and_load_model(self, filename):
+		self.__add_lstm_90lr_sp500_layers()
+		self.model.load_weights(filename)
 
 	def write_to_json(self):
 		# serialize model to JSON
@@ -78,12 +140,24 @@ class Neural_Network():
 		self.model.save_weights("model.h5")
 		print("Saved model to disk")
 
-	def load_weights_from_h5(self):
+	def load_weights_from_h5(self, file_path):
 		# Model needs to be built first
-		self.model.load_weights("model.h5")
+		self.model.load_weights(file_path)
 
 	def predict(self):
-		print()
-
+		return self.model.predict(self.x_test_)
+	
 	def summary(self):
 		self.model.summary()
+
+	def write_history_to_file(self):
+		numpy_loss_history = np.array(loss_history)
+		np.savetxt("loss_history.txt", numpy_loss_history, delimiter=",")
+	# def write_accuracy(self):
+		# plt.plot(self.history_.history['acc'])
+		# plt.plot(self.history_.history['val_acc'])
+		# plt.title('model accuracy')
+		# plt.ylabel('accuracy')
+		# plt.xlabel('epoch')
+		# plt.legend(['train', 'test'], loc='upper left')
+		# plt.show()
