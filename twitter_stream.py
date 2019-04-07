7fed9165d649196c9e7c953258f7e2f6ddd7d860
Jacob McKenna <mcjacob61@gmail.com>
2019-04-07 13:06:10 -0800

    added select users tweets, can now pull specific users of up to 200 tweets only.

--- twitter_stream.py
+++ twitter_stream.py
@@ -18,8 +18,14 @@ tweets = []
 CONN = psycopg2.connect(host='localhost', user='stock', password='money', dbname='stock_market_data')
 CURS = CONN.cursor()
 INSERT_QUERY = """INSERT INTO \"Tweets\" 
-				(text, location, user_id, retweeted, retweet_count, favorite_count, reply_count, quote_count, utc_date_created) 
+				(text, location, user_id, retweeted, retweet_count, 
+				favorite_count, reply_count, quote_count, utc_date_created) 
 				VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+
+INSERT_USER_QUERY = """INSERT INTO \"User_Tweets\" 
+				(text, location, user_id, retweeted, retweet_count, 
+				favorite_count, utc_date_created) 
+				VALUES (%s, %s, %s, %s, %s, %s, %s)"""
 
 class TweetStreamListener(tweepy.StreamListener):
 
@@ -29,11 +35,13 @@ class TweetStreamListener(tweepy.StreamListener):
 		self.api = api or API()
 	
 	def on_status(self, status):
+		print(status._json)
+		print('\n'*20)
 		retweeted_full_text = status._json.get('retweeted_status')
 		retweeted = False
 
 		try:
-			text = status.extended_tweet["full_text"]
+			text = status.extended_tweet['full_text']
 			# print(text)
 			retweeted = True
 		except AttributeError:
@@ -54,7 +62,10 @@ class TweetStreamListener(tweepy.StreamListener):
 					status.quote_count,
 					str(date)))
 
+		print(len(tweets))
 		if len(tweets) == 10:
+			print('Inserting Batch: ' + str(self.BATCH_COUNTER))
+			self.BATCH_COUNTER += 1
 			self.write_to_postgres(tweets)
 		
 
@@ -64,7 +75,6 @@ class TweetStreamListener(tweepy.StreamListener):
 			return False
 	
 	def write_to_postgres(self, data):
-		print("Inserting Batch: " + str(self.BATCH_COUNTER))
 		for tweet in data:
 			try:
 				CURS.execute(INSERT_QUERY, tweet)
@@ -72,7 +82,7 @@ class TweetStreamListener(tweepy.StreamListener):
 				print(error)
 		CONN.commit()
 		tweets.clear()
-		self.BATCH_COUNTER += 1
+
 
 # Remove newlines. 
 def strip_list_newline(str_to_strip):
@@ -88,28 +98,88 @@ def authenticate():
 	api = tweepy.API(auth)
 	return api
 
-def main():
+def stream():
 	streamer = TweetStreamListener()
 	api = authenticate()
 
 	myStream = tweepy.Stream(auth=api.auth, listener=streamer)
-	# Idea: Use newspapers and keywords to fill lists and listen
-	# I'll have to figure out what I need to do to get special keywords, this might 
-	# include a very generic approach.
-	# Use below of async connection, otherwise main thread is used. 
-	# myStream.filter(track=['python'], async=True) 
-	special_words = ['NVDA', 'AAPL', 'Apple', 'market', 'stocks', 'bull', 'bullish', 'bear', 'bearish']
 
-	# Used http://boundingbox.klokantech.com/ to filter NYC tweets about the stock market. 
-	# May use multiple locations, filter by news sources/people/etc. 
-	NYC = [-122.48, 47.79, -79.15, -122.48]
-	# Location, because it's not well documented in the source, takes two coordinate position per point (4 total).
-	# Still does not filter everything not related to the stock market. 
+	special_words = ['AAPL', 'Apple', 'market', 'stocks', 'bull', 'bullish', 'bear', 'bearish']
 
-
-	# TODO: Filter by english documents.   
 	myStream.filter(languages=["en"], track=special_words)
-	# myStream.userstream(track=special_words)
+
+
+
+
+
+def on_status(statuses):
+	tweets = []
+	for status in statuses: 
+		full_text = status._json.get('full_text')
+		print(full_text)
+		retweeted = False
+
+		try:
+			retweeted = status._json.get('retweeted')
+			print(retweeted)
+		finally:
+			print('yuppers')
+
+		date = status.created_at
+		# date = datetime.strftime('%Y-%M-%D')
+
+		tweets.append(
+					(full_text, 
+					status.user.location, 
+					status.id, 
+					retweeted,
+					status.retweet_count,
+					status.favorite_count,
+					str(date)))
+
+	write_to_postgres(tweets)
+
+def write_to_postgres(tweets):
+	for tweet in tweets:
+		try:
+			CURS.execute(INSERT_USER_QUERY, tweet)
+		except psycopg2.Error as error:
+			# Likely over 300 chars.
+			print(error)
+
+	CONN.commit()
+	tweets.clear()
+
+def get_users_tweets():
+	api = authenticate()
+
+	# Both former and present
+	apple_executives = [1636590253, 17104751, 754500, 20832113, 38854434,
+						22938914, ]
+
+	apple_pundits_analysts = [12131132, 33423, 9324442, 1835411, 14331688, 15439395, 134665872]
+
+	# 44764761 special case The Loop Media
+	apple_journalists = [2960721, 44764761]
+
+	users = apple_executives + apple_pundits_analysts + apple_journalists
+	users_tweets = []  
+
+	# Maximum 200 tweets may be extracted at a time, starting with earliest. 
+	number_of_tweets=200
+	for user in users:
+		users_tweets.append(api.user_timeline(user_id=user, count=number_of_tweets, tweet_mode='extended')) 
+
+	# create array of tweet information: username,  
+	# tweet id, date/time, text
+	for tweets in users_tweets: 
+		# print(tweets)
+		on_status(tweets)
+
+
+def main():
+	# stream()
+	get_users_tweets()
 
 if __name__ == '__main__':
 	main()
