216f09e6c9f8d26b91c409b4915c90806a63d038
Jacob McKenna <mcjacob61@gmail.com>
2020-02-29 16:26:08 -0900

    Removed useless code. Merged positive/negative sentiments into a single dataframe. Next step is to convert dataframe into daily segments.

--- data_processor.py
+++ data_processor.py
@@ -18,162 +18,99 @@ import numpy as np
 import sys
 import datetime
 
-vectorizer = CountVectorizer(stop_words=["i", "me", "my", "myself", "we", "our", "ours", 
-"ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", 
-"she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", 
-"themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", 
-"are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", 
-"doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", 
-"by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", 
-"above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", 
-"further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", 
-"few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", 
-"own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"])
-
 DOTCSV = '.csv'
 DOTTXT = '.txt'
 
 DATA_DIR = 'data/stocknet-dataset/'
 PRICE_PREPROCESSED_DIR = DATA_DIR + 'price/preprocessed/'
 TWEET_PREPROCESSED_DIR = DATA_DIR + 'tweet/preprocessed/'
+TWEET_RAW_DIR = DATA_DIR + 'tweet/raw/'
 
 tb = Blobber(analyzer=NaiveBayesAnalyzer())
 
-class Data_Processor(object):
+class TweetFrame(object):
 
     def __init__(self, symbol):
+        # Init
         csv_file = PRICE_PREPROCESSED_DIR + symbol + DOTTXT
         tweets_folder = TWEET_PREPROCESSED_DIR + symbol+ '/' 
 
-        self.symbol_times_series = None
-        try:
-            self.symbol_times_series = pd.read_csv(csv_file, sep=" ")
-        except:
-            print(f"Error for {symbol}.")
-            print(sys.exc_info())
-            sys.exit()
+        self.times_series_ = self.__set_price_data(symbol, csv_file)
+        self.user_tweets_ = self.__set_tweet_data(symbol, tweets_folder)
 
-        self.user_tweets_ = None
+        # Finalize a prepared dataset
+        self.nn_ready_data = pd.DataFrame()
+
+        pos_vals, neg_vals = self.__determine_tweet_sentiment()
+        self.nn_ready_data['pos_val'] = pos_vals
+        self.nn_ready_data['neg_val'] = neg_vals
+        self.nn_ready_data['date'] = self.user_tweets_['created_at'].dt.date
+        
+    def __format_date(self):
+        return self.user_tweets_['created_at'].date()
+            
+    def __set_tweet_data(self, symbol, folder):
         try:
             # Store in list first.
             tmp_list = []
-            for filename in os.listdir(tweets_folder):
-                df = pd.read_json(tweets_folder + filename, lines = True)
+            for filename in os.listdir(folder):
+                df = pd.read_json(folder + filename, lines = True)
                 tmp_list.append(df)
-
-            self.user_tweets_ = pd.concat(tmp_list, axis=0, ignore_index = True)
+            return pd.concat(tmp_list, axis=0, ignore_index = True)
 
         except FileNotFoundError:
             print(f"""
             No file for symbol \"{symbol}\". 
-            File name/location: {tweets_folder}""")
+            File name/location: {folder}""")
             sys.exit()
        
         except:
-            print(f"Error for {symbol}.")
+            print(f"Error in __set_tweet_data for {symbol}.")
+            print(sys.exc_info()[1])
+            sys.exit()
+
+    def __set_price_data(self, symbol, file):
+        try:
+            return pd.read_csv(file, sep=" ")
+        except:
+            print(f'Error in __set_price_data for {symbol}.')
             print(sys.exc_info())
             sys.exit()
-        
-        self.user_tweets_word_counts_ = None
-        self.user_tweets_document_count_ = 0
-        self.user_tweets_word_index_ = None
-        self.user_tweets_word_docs_ = None
-        self.vectorized_user_tweets_ = None
-
-        self.user_tweets_sentiment_ = []
-
-        # Sanity checks.
-        assert self.user_tweets_.columns[0] == "created_at", f"Expected \"created_at\", got {self.user_tweets_.columns[0]}"
-
-
-    def __summarize_user_tweets(self, tweets):
-        t = Tokenizer()
-        t.fit_on_texts(tweets)
-
-        self.user_tweets_word_counts_ = t.word_counts
-        self.user_tweets_document_count_ = t.document_count
-        self.user_tweets_word_index_ = t.word_index
-        self.user_tweets_word_docs_ = t.word_docs
     
-    def print_tweet_counts(self):
-        print("Select User Tweets", self.user_tweets_document_count_)
+    def __calculate_pos_neg_ratio(self):
+        P100 = 1.0
+        df = self.nn_ready_data
+        num_of_rows = df.shape[0]
+        num_of_pos_tweets = len(df[df['pos_val'] > .5 ].values.tolist())
+        pos_ratio = num_of_pos_tweets / num_of_rows
+        neg_ratio = P100 - pos_ratio 
+        return pos_ratio, neg_ratio
 
-    def print_user_tweets(self):
-        print(self.user_tweets_)
-
-    def print_numerical(self):
-        print(self.symbol_times_series)
+    def print_tweets_statistics(self):
+        pos_ratio, neg_ratio = self.__calculate_pos_neg_ratio()
+        print('Number of tweets: ', len(self.user_tweets_))
+        print('Document Count:', self.user_tweets_document_count_)
+        print('Dataframe stats: ', self.nn_ready_data.describe())
+        print('Percent Tweets that are positive: ', pos_ratio)
+        print('Percent Tweets that are negative: ', neg_ratio)
     
-    def print_streamed_tweets(self):
-        print(self.streamed_tweets_)
-
-    def print_vectorized_contents(self):
-        print("User vector length: ", len(self.vectorized_user_tweets_))
-        print("Streamed vector length: ", len(self.vectorized_streamed_tweets_))
-
-    def __remove_tweet_urls(self, tweets):
-        return [re.sub(r"- http\S+", "", tweet) for tweet in tweets]
-
-    def __vectorize_tweets(self, tweets):
-        vec = vectorizer.fit_transform(tweets)
-        tmp_list = vec.toarray()
-        return tmp_list
-
-    def __get_tweet_sentiment(self, tweets):
-        tb = Blobber(analyzer=NaiveBayesAnalyzer())
-        # List of Sentiment objects
-        return [tb(tweet).sentiment for tweet in tweets]
-    
-    def __get_sentiment_values(self, sentiment_list):
-        POS = 1
-        NEG = 0
-        CLASSIFICATION = 0
+    def __determine_tweet_sentiment(self):
         POS_SENTIMENT = 1
         NEG_SENTIMENT = 2
 
+        tb = Blobber(analyzer=NaiveBayesAnalyzer())
         pos_values = []
         neg_values = []
-        clafficiations = []
+ 
+        for tweet in self.user_tweets_['text'].tolist():
+            tweet = ' '.join(tweet)
+            #Sentiment object.
+            sentiment = tb(tweet).sentiment
+            pos_values.append(sentiment[POS_SENTIMENT])
+            neg_values.append(sentiment[NEG_SENTIMENT])
 
-        for sentiment in sentiment_list:
-            if sentiment[CLASSIFICATION] == 'pos':
-                clafficiations.append(POS)
-                pos_values.append(sentiment[POS_SENTIMENT])
-                neg_values.append(sentiment[NEG_SENTIMENT])
-            else:
-                clafficiations.append(NEG)
-                neg_values.append(sentiment[NEG_SENTIMENT])
-                pos_values.append(sentiment[POS_SENTIMENT])
-
-        return pos_values, neg_values, clafficiations
-
-    def __index_at_zero(self, dt_time):
-        return 10000*dt_time.year + 100*dt_time.month + dt_time.day
-
-    def __build_file_format_string(self, filename, batch_num, file_set):
-        return filename + file_set + str(batch_num) + DOTCSV
-
-    def process(self):
-        print()
-        #self.__clean_tweets()
-
-    def __clean_tweets(self):
-        # Remove row and bad data if any.
-        self.user_tweets_ = self.user_tweets_.drop(columns=['row'])
-        self.user_tweets_ = self.user_tweets_.dropna()
-
-        # Remove HTTPS from tweets.
-        tweets = self.user_tweets_['text']
-        tweets = self.__remove_tweet_urls(tweets)
-
-        # Assign new text back to dataframe.
-        self.user_tweets_['text'] = tweets
-        self.vectorized_user_tweets_ = self.__vectorize_tweets(tweets)
-
-        # List of tweet sentiments.
-        self.user_tweets_sentiment_ = self.__get_tweet_sentiment(tweets)
-        self.__summarize_user_tweets(tweets)
+        return pos_values, neg_values
 
 
-data = Data_Processor("AAPL")
-data.process()
+data = TweetFrame('AAPL')
+data.print_tweets_statistics()
